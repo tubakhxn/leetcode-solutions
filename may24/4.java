f6b71dc8e3560ffa1d0fcaf1e0d4c869dd38db7d
Tuba Khan <122105012+tubakhxn@users.noreply.github.com>
2024-05-05 01:51:25 +0530

    Create 4.java

---
+++ may24/4.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1, count = 0;
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) i++;
+            count++;
+            j--;
+        }
+        return count;
+    }
+}
