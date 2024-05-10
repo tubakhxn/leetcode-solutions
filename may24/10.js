5dca247bfc17d7db3a78e673402334a1a602da3f
Tuba Khan <122105012+tubakhxn@users.noreply.github.com>
2024-05-10 20:06:26 +0530

    Create 10.js

---
+++ may24/10.js
@@ -0,0 +1,12 @@
+
+var kthSmallestPrimeFraction = function (arr, k) {
+    let fractions = [], ans = []
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            fractions.push([arr[i] / arr[j], arr[i], arr[j]])
+        }
+    }
+    fractions.sort((a, b) => a[0] - b[0]);
+
+    return [fractions[k - 1][1], fractions[k - 1][2]]
+};
